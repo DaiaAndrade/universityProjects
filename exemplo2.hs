66e44c254fb55246fe20546b6b7bca362f5f5731
Daiane Andrade <DaiaAndrade@users.noreply.github.com>
2015-10-22 10:36:18 -0200

    commit2

---
+++ exemplo2.hs
@@ -0,0 +1,18 @@
+--22Out
+data Arvore = Nodo Int Arvore Arvore | Folha Int
+				deriving(Eq, Show)
+
+
+arv1 :: Arvore 
+arv1 = Nodo 3 (Nodo 9 (Folha 2) (Folha 2)) (Nodo 7 (Folha 1) (Folha 33))
+
+arv2 :: Arvore
+arv2 = Nodo 6 (Folha 1) (Nodo 44 (Folha 97) (Folha 92))
+
+somaArvore :: Arvore -> Int
+somaArvore (Folha n) = n
+somaArvore (Nodo n a1 a2) = n + somaArvore a1 + somaArvore a2
+
+arvoreLista :: Arvore -> [Int]
+arvoreLista (Folha n) = [n]
+arvoreLista (Nodo n a1 a2) = n : arvoreLista a1 ++ arvoreLista a2
