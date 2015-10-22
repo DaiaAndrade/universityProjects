7ba5bf2896427ba1f5e7ee4e2b150181851bdefd
Daiane Andrade <DaiaAndrade@users.noreply.github.com>
2015-10-22 11:31:30 -0200

    bigstep

--- exemplo2.hs
+++ exemplo2.hs
@@ -20,3 +20,32 @@ arvoreLista (Nodo n a1 a2) = n : arvoreLista a1 ++ arvoreLista a2
 refleteArvore :: Arvore -> Arvore
 refleteArvore (Folha n) = Folha n
 refleteArvore (Nodo n a1 a2) = Nodo n (refleteArvore a2) (refleteArvore a1)
+
+data E = Num Int | Soma E E | Mult E E
+			deriving (Eq, Show)
+prog1 :: E
+prog1 = Mult (Soma (Num 3) (Num 4)) (Soma (Num 2) (Num 1))
+
+bigStep :: E -> Int
+bigStep (Num n) = n
+bigStep (Soma e1 e2) = bigStep e1 + bigStep e2
+bigStep (Mult e1 e2) = bigStep e1 * bigStep e2
+
+data B = TRUE | FALSE | Nao B | And B B | Ou B B
+		deriving (Eq, Show)
+
+bigStep2 :: B -> Bool
+bigStep2 TRUE = True
+bigStep2 FALSE = False
+bigStep2 (Nao TRUE) = False
+bigStep2 (Nao FALSE) = True
+bigStep2 (Nao b) = not (bigStep2 b)
+bigStep2 (And TRUE b2) = bigStep2 b2
+bigStep2 (And FALSE _) = False
+bigStep2 (And b1 b2) = (bigStep2 b1) && (bigStep2 b2)
+bigStep2 (Ou TRUE _) = True
+bigStep2 (Ou FALSE b2) = bigStep2 b2
+bigStep2 (Ou b1 b2) = (bigStep2 b1) || (bigStep2 b2)
+
+prog2 :: B
+prog2 = Nao (Ou (And (TRUE) (FALSE)) (And (FALSE) (FALSE)))
