85cba1f8a73dee07b945b1f940fc1d032bf689f5
Daiane Andrade <DaiaAndrade@users.noreply.github.com>
2015-12-27 19:38:41 -0200

    Create main.py

---
+++ IA-Pygames/main.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pygame, sys
+from pygame.locals import *
+from maps import *
+
+pygame.init()
+screen = InitialScreen()
+screen.loop()
