95a0912332de035d3733a4d09c94bba9e1c2134a
Daiane Andrade <DaiaAndrade@users.noreply.github.com>
2017-05-31 09:17:24 -0300

    Create Fibo_openMP.cpp

---
+++ IPPD/Fibo_openMP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+unsigned long int Fibo (unsigned long int n) {
+    return (n >= 2 ? Fibo(n-1) + Fibo(n-2) : n);
+}
+
+int main(){
+    int i = 0;
+    unsigned long int vet[100];
+    cout << "Oi" << endl;
+    #pragma omp parallel
+    {
+        #pragma omp for schedule(static) lastprivate (vet,i)
+            for (i = 0; i < 100; i++){
+                vet[i] = Fibo(40);
+                cout << i << endl;
+            }
+    }
+}
