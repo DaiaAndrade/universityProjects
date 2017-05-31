a5fa4603ce59bf4b467792ed88bafb640bee7dca
Daiane Andrade <DaiaAndrade@users.noreply.github.com>
2017-05-31 16:37:03 -0300

    Update Fibo_openMP.cpp

--- IPPD/Fibo_openMP.cpp
+++ IPPD/Fibo_openMP.cpp
@@ -9,13 +9,11 @@ unsigned long int Fibo (unsigned long int n) {
 int main(){
     int i = 0;
     unsigned long int vet[100];
-    cout << "Oi" << endl;
     #pragma omp parallel
     {
         #pragma omp for schedule(static) lastprivate (vet,i)
             for (i = 0; i < 100; i++){
                 vet[i] = Fibo(40);
-                cout << i << endl;
             }
     }
 }
